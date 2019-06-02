528aa538cce8d9d3c38ba5fb15ce0ec14f7bea04
newtonjose <josephnewton001@gmail.com>
2019-06-02 14:19:15 -0300

    removendo caracteres 'estranhos'

--- src/main/java/megasena/MegaSena.java
+++ src/main/java/megasena/MegaSena.java
@@ -14,20 +14,23 @@ import megasena.ProcessaMegaSenaHTML;
 // TODO: Acrescentar servicos de leitura de resultados.
 // TODO: Integrar descompressao do arquivo.
 
-/** Esta classe tem como finalidade ilustrar o emprego de 
+/**
+ * Esta classe tem como finalidade ilustrar o emprego de
  * ProcessaMegaSenaHTML, que tem por objetivo processar uma arquivo
  * HTML fornecido pela CEF contendo resultados da Mega Sena. Cada
- * resultado (sorteio) � depositado em uma inst�ncia da classe Sorteio
- * e exibido na sa�da padr�o. 
+ * resultado (sorteio) é depositado em uma instáncia da classe Sorteio
+ * e exibido na saída padrão.
  *
- * 
+ * @author Administrator e
+ * <a href="https://github.com/newtonjose">newtonjose</a>
+ * @version 1.1
  */
 public class MegaSena implements IProcessaSorteio {
-    
+
     public void processaSorteio(megasena.Sorteio s) {
         System.out.println(s);
     }
-    
+
     public static void main(String[] args) throws Exception {
         MegaSena ms = new MegaSena();
         ProcessaMegaSenaHTML pms = new ProcessaMegaSenaHTML();

dbff507f634970af4f05c94e11d566fb0d81c90c
newtonjose <josephnewton001@gmail.com>
2019-06-02 14:20:01 -0300

    documentação dos métodos

--- src/main/java/megasena/MegaSena.java
+++ src/main/java/megasena/MegaSena.java
@@ -27,10 +27,21 @@ import megasena.ProcessaMegaSenaHTML;
  */
 public class MegaSena implements IProcessaSorteio {
 
+    /**
+     * Método que implementa o processamento de um sorteiro de forma
+     * inteligente.
+     * @param s Tipo megasena.Sorteio.
+     */
     public void processaSorteio(megasena.Sorteio s) {
         System.out.println(s);
     }
 
+    /**
+     * Método main implementa a instanciação das classes MegaSena e
+     * ProcessaMegaSenaHTML e o HTML da página MegaSena.
+     * @param args Array de parâmetros do tipo String.
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         MegaSena ms = new MegaSena();
         ProcessaMegaSenaHTML pms = new ProcessaMegaSenaHTML();

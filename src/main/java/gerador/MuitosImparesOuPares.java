67101b6ce25fec133c35e4db1c861f88299bdff4
CauaRebelo <cauarebelo@discente.ufg.br>
2019-06-03 10:14:01 -0300

    Documenta a função MuitoImparesOuPares
    Corrige erros na copyright também

--- src/main/java/gerador/MuitosImparesOuPares.java
+++ src/main/java/gerador/MuitosImparesOuPares.java
@@ -1,14 +1,24 @@
 /*
  * Copyright (c) 2018.
  *
- * Fábio Nogueira de Lucena
- * Fábrica de Software - Instituto de Informática (UFG)
+ * Fabio Nogueira de Lucena
+ * Fabrica de Software - Instituto de Informatica (UFG)
  *
  */
 
 package gerador;
-
+/**
+ * Identifica se o jogo tem muitos pares ou impares, para saber se deve
+ * ser eliminado ou nao.
+*/
 public class MuitosImparesOuPares implements Exclusao {
+	/**
+	 * Descobre se o jogo deve ser eliminado/desqualificado
+	 * @param jogo O jogo de megasena a ser identificado
+     * @param dezenas As dezenas do jogo onde serao identificado os pares
+     * @return Retorna verdadeiro se o jogo tiver muitos impares ou muitos pares,
+     * ou seja, se tem >= 5 pares ou impares, ou falso se não satisfazer isso.
+	*/
 	public boolean exclui(int jogo, int[] dezenas) {
 		int pares = 0;
 		for (int dezena : dezenas) {

5f9786d647c441c01a90ed1049b51f467dcf7318
jc00973 <fonseca.jc@live.com>
2019-06-02 22:38:53 -0300

    Documentação implementada.

--- src/main/java/gerador/TodosOsNumerosEmFaixas.java
+++ src/main/java/gerador/TodosOsNumerosEmFaixas.java
@@ -12,6 +12,17 @@ import java.util.Arrays;
 
 public class TodosOsNumerosEmFaixas implements Exclusao {
 
+	/**
+	 * O método verifica se as dezenas de um jogo estão todas em um mesmo intervalo:
+	 * de 1 ao 20, de 21 ao 40 ou de 41 ao 60.
+	 *
+	 * @param jogo Jogo que terá suas dezenas analisadas.
+	 * @param dezenas Vetor com as dezenas do jogo que serão analisadas.
+	 *
+	 * @return Verdadeiro caso as dezenas estejam todas em um mesmo intervalo, falso caso
+	 * não estejam.
+	 */
+
 	public boolean exclui(int jogo, int[] dezenas) {
 		Arrays.sort(dezenas);
 		if (dezenas[5] <= 20 || dezenas[0] >= 41)
